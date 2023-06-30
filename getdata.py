2d347b23fe8d70e6ac0de4bbf9274bbf60d98e03
M K <mkpro118@gmail.com>
2023-07-01 01:55:21 +0530

    major refactor, implemented a public api with the `getdata` generator

--- getdata.py
+++ getdata.py
@@ -1,28 +1,73 @@
 import requests
-import keys
+from typing import Generator
 
-CLIENT_ID = keys.CLIENT_ID
-SECRET_KEY = keys.SECRET_KEY
-USERNAME = keys.USERNAME
-PASSWORD = keys.PASSWORD
-#Set this to desired subreddit (ex. news NOT r/news)
-SUBREDDIT = 'showerthoughts'
+from ShowerThoughtsRedditAPI import keys
 
-headers = {'User-Agent': 'EducationalAPI/0.0.1'}
-auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
-data = {
-    'grant_type': 'password',
-    'username': USERNAME,
-    'password': PASSWORD
-}
-req = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
-TOKEN = req.json()['access_token']
-headers['Authorization'] = "bearer " + TOKEN
 
-link = 'https://oauth.reddit.com/r/' + SUBREDDIT + '/top'
-req = requests.get(link, headers=headers)
+__all__ = (
+    'getdata',
+    'DEFAULT_SUBREDDIT',
+)
 
-for post in req.json()['data']['children']:
-    title = post['data']['title']
-    print(title)
-    
+DEFAULT_SUBREDDIT = 'showerthoughts'
+HeaderGenerator = Generator[dict, None, None]
+DataGenerator = Generator[str, None, None]
+
+
+def __headers__(CLIENT_ID: str = keys.CLIENT_ID,
+                SECRET_KEY: str = keys.SECRET_KEY,
+                USERNAME: str = keys.USERNAME,
+                PASSWORD: str = keys.PASSWORD) -> HeaderGenerator:
+    '''Generate required auth information'''
+    headers = {'User-Agent': 'EducationalAPI/0.0.1', }
+
+    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+
+    data = {
+        'grant_type': 'password',
+        'username': USERNAME,
+        'password': PASSWORD,
+    }
+
+    with requests.post('https://www.reddit.com/api/v1/access_token',
+                       auth=auth, data=data, headers=headers) as response:
+        TOKEN = response.json()['access_token']
+
+    headers['Authorization'] = f'bearer {TOKEN}'
+
+    while True:
+        yield headers
+
+
+def getdata(subreddit: str,
+            headers: HeaderGenerator = __headers__()) -> DataGenerator:
+    subreddit = subreddit.strip('/')
+    if '/' in subreddit:
+        if not subreddit.startswith('r/'):
+            raise ValueError(f'{subreddit} is not a valid subreddit.')
+        subreddit = subreddit[2:]
+
+    url = f'https://oauth.reddit.com/r/{subreddit}/top'
+
+    with requests.get(url, headers=next(headers)) as response:
+        assert response.ok, f'{url} returned a status_code={response.status_code}'
+
+        return (
+            post['data']['title'] for post in response.json()['data']['children']
+        )
+
+
+# ensure keys are never used again
+del keys
+del HeaderGenerator
+del DataGenerator
+
+if __name__ == '__main__':
+    import sys
+
+    subreddit = DEFAULT_SUBREDDIT
+
+    if len(sys.argv) > 1:
+        subreddit = sys.argv[1]
+
+    *map(print, getdata(subreddit))
