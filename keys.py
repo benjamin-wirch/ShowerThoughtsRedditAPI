a2a11922a5150c6a6b1b6c0a4613f85564acbbf4
M K <mkpro118@gmail.com>
2023-07-01 01:53:39 +0530

    Additional security using environment variables

--- keys.py
+++ keys.py
@@ -1,6 +1,14 @@
-#Input credentials here
+import os
 
-CLIENT_ID = ''
-SECRET_KEY = ''
-USERNAME = ''
-PASSWORD = ''
+__all__ = (
+    'CLIENT_ID',
+    'SECRET_KEY',
+    'USERNAME',
+    'PASSWORD',
+)
+
+
+CLIENT_ID: str = os.environ['REDDIT_API_CLIENT_ID']
+SECRET_KEY: str = os.environ['REDDIT_API_SECRET_KEY']
+USERNAME: str = os.environ['REDDIT_API_USERNAME']
+PASSWORD: str = os.environ['REDDIT_API_PASSWORD']
